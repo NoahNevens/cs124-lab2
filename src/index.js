9e9ac44c47e59f1faeaa106ef7fd993d6f3a36a4
danielasechen <dsechen@hmc.edu>
2022-03-09 14:24:59 -0800

    initialized firestore

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD8pvUeQT320PvjuCSg4mRErbvDaQ_xjFg",
+    authDomain: "cs124-lab3-dan.firebaseapp.com",
+    projectId: "cs124-lab3-dan",
+    storageBucket: "cs124-lab3-dan.appspot.com",
+    messagingSenderId: "1036883305737",
+    appId: "1:1036883305737:web:2d7bd2360059043e2e9fd8"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const initialData = [
     {
