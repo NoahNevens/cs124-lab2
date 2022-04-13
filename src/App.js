fcba678a0a90a0833b38649a5d72a9a1fc1ac045
noahnevens <nnevens@g.hmc.edu>
2022-04-13 15:59:30 -0700

    Lots of css to do, mainly trying to figure out other stuff

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import LoadingScreen from "./LoadingScreen.js";
 import LoginPage from "./LoginPage.js"
 import SignedInApp from "./SignedInApp.js";
 import {useAuthState, useSignInWithEmailAndPassword, useSignInWithGoogle} from "react-firebase-hooks/auth";
-
+import {useCreateUserWithEmailAndPassword} from "react-firebase-hooks/auth";
 
 function App(props) {
     const [user, loading, error] = useAuthState(props.auth);
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,7 +1,11 @@
 import "./LoginPage.css";
 import Header from "./Header.js"
 import {useState} from "react";
-import {useSignInWithEmailAndPassword, useSignInWithGoogle} from "react-firebase-hooks/auth";
+import {
+    useCreateUserWithEmailAndPassword,
+    useSignInWithEmailAndPassword,
+    useSignInWithGoogle
+} from "react-firebase-hooks/auth";
 
 function SignInWithGoogle(props) {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(props.auth);
@@ -17,16 +21,87 @@ function SignInWithEmail(props) {
 
     const [email, setEmail] = useState("");
     const [pw, setPw] = useState("");
-    // *** NEED TO DO EMAIL AND PASSWORD
-    // reference this? https://github.com/nrhodes-hmc/cs124sp22-exercises/blob/20-authentication-solution/src/App.js
+    if (loading) {
+        return <p>Logging in...</p>
+    }
 
-    return <button className="login_button">
+    return <button className="login_button"
+                   onClick={() => props.onLogInPage(true)}>
         Sign In With Email
     </button>
 }
 
+function SignInWithEmailPopUp(props) {
+    const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(props.auth);
+
+    const [email, setEmail] = useState("");
+    const [pw, setPw] = useState("");
+    if (loading) {
+        return <p>Logging in...</p>
+    }
+
+    return <>
+        <div className="overlay">
+        </div>
+        <div className="signInContainer">
+            <div id="signInBox">
+                <h1 id="enterCreds">Enter Credentials</h1>
+                {error && <p>"Error logging in: " {error.message}</p>}
+                <label htmlFor='email'>email: </label>
+                <input type="text" id='email' value={email}
+                       onChange={e=>setEmail(e.target.value)}/>
+                <br/>
+                <label htmlFor='pw'>pw: </label>
+                <input type="text" id='pw' value={pw}
+                       onChange={e=>setPw(e.target.value)}/>
+                <br/>
+                <button className="signin_button"
+                        onClick={() => signInWithEmailAndPassword(email, pw)}>
+                    Sign In
+                </button>
+            </div>
+        </div>
+        </>
+}
+
+function SignUp(props) {
+    const [createUserWithEmailAndPassword, userCredential, loading, error] = useCreateUserWithEmailAndPassword(props.auth);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    if (userCredential) {
+        return <div>Signed In Already!</div>
+    } else if (loading) {
+        return <p>Signing Up...</p>
+    }
+
+    return <div>
+        {error && <p>"Error signing up: " {error.message}</p>}
+        <label htmlFor='email'>Email: </label>
+        <input type="text" id='email' value={email}
+               onChange={e=>setEmail(e.target.value)}/>
+        <br/>
+        <label htmlFor='pw'>Password: </label>
+        <input type="text" id='pw' value={password}
+               onChange={e=>setPassword(e.target.value)}/>
+        <br/>
+        <button onClick={() =>
+            createUserWithEmailAndPassword(email, password)}>
+            Sign Up
+        </button>
+    </div>
+}
+
 function LoginPage(props) {
-    return <div className="frontpage">
+    const [loginPage, setLoginPage] = useState(false);
+
+    function handleSetLoginPage(bool) {
+        setLoginPage(bool);
+        console.log(loginPage);
+    }
+
+    return <>
+        <div className="frontpage">
         <div id="login_header">
             <Header />
         </div>
@@ -35,10 +110,13 @@ function LoginPage(props) {
             <h2>Sign In</h2>
         </div>
         <div id="login_buttons">
-            <SignInWithEmail auth={props.auth} />
+            <SignInWithEmail auth={props.auth} onLogInPage={handleSetLoginPage}/>
             <SignInWithGoogle auth={props.auth} />
+            <SignUp auth={props.auth} />
         </div>
     </div>
+        {loginPage && <SignInWithEmailPopUp auth={props.auth} onLogInPage={handleSetLoginPage}/>}
+    </>
 
 }
 
