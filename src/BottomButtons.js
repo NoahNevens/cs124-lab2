162ab28b9ec67fd15c7548d37fba7b2e7f64dda3
noahnevens <nnevens@g.hmc.edu>
2022-05-06 15:15:16 -0700

    Two things done

--- src/BottomButtons.js
+++ src/BottomButtons.js
@@ -7,10 +7,12 @@ function BottomButtons(props) {
         <button type="radio"
                 className={props.isHideCompleted ? "bottom_button_new" : "bottom_button_reg"}
                 id="show_uncomplete"
-                onClick={props.onToggleCompletedItems} >
+                onClick={props.onToggleCompletedItems}
+                tabIndex={props.popup ? -1 : 0}>
             {props.isHideCompleted ? "Show All" : "Hide Completed"}</button>
         <button type="radio" className="bottom_button_reg" id="clear_complete"
-                onClick={props.onClearCompletedItems} >
+                onClick={props.onClearCompletedItems}
+                tabIndex={props.popup ? -1 : 0}>
             Clear Completed</button>
     </div>;
 }
